7ba3d7848d2e1d5922ccc3f71ca5bd3ddcd5e2f6
din1998 <dinislam8803@gmail.com>
2023-01-24 23:31:23 +0600

    add box icon

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import '../StylesFile/about.css';
 import { Row,Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faCloud,faChartLine,faPlay,faChartPie,faUserGroup,faShareNodes,faRobot,faMobile,faWindowRestore } from '@fortawesome/free-solid-svg-icons'
 
 export default function About() {
   return(
@@ -10,52 +10,76 @@ export default function About() {
         <Row>
             <Col md={3}><div className='box box-a'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />AWS</li>
+                  <li><FontAwesomeIcon icon={faCloud} className="title__icon" />AWS</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-b'>
                 <div className='box__text'>
-                <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Advanced Analytics</li>
+                <li><FontAwesomeIcon icon={faChartLine} className="title__icon" />Advanced Analytics</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-c'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Basic Analytics</li>
+                  <li><FontAwesomeIcon icon={faChartPie} className="title__icon" />Basic Analytics</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-d'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Consulting</li>
+                  <li><FontAwesomeIcon icon={faUserGroup} className="title__icon" />Consulting</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
         </Row>
         <Row>
             <Col md={3}><div className='box box-b'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Data Science</li>
+                  <li><FontAwesomeIcon icon={faShareNodes} className="title__icon" />Data Science</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-a'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />loT/Robotics</li>
+                  <li><FontAwesomeIcon icon={faRobot} className="title__icon" />loT/Robotics</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-d'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Mobile Application</li>
+                  <li><FontAwesomeIcon icon={faMobile} className="title__icon" />Mobile Application</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
             <Col md={3}><div className='box box-c'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faPlay} className="title__icon" />Web Application</li>
+                  <li><FontAwesomeIcon icon={faWindowRestore} className="title__icon" />Web Application</li>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                  <div className="links">
+                    <a href='#' className='link'>See More</a>
+                    </div>
                 </div>
               </div></Col>
         </Row>
