2b748ff60406175d46336e51cbe130321a77116c
din1998 <dinislam8803@gmail.com>
2023-01-26 21:29:48 +0600

    add responsive navBar

--- src/components/About.js
+++ src/components/About.js
@@ -1,90 +1,104 @@
 import '../StylesFile/about.css';
-import {Container, Row,Col } from 'react-bootstrap';
+
+import Container from '@mui/material/Container';
+import {Grid} from '@mui/material'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloud,faChartLine,faPlay,faChartPie,faUserGroup,faShareNodes,faRobot,faMobile,faWindowRestore } from '@fortawesome/free-solid-svg-icons'
+import { faCloud,faChartLine,faPlay,faChartPie,faUserGroup,faShareNodes,faRobot,faMobile,faWindowRestore, faWindowClose } from '@fortawesome/free-solid-svg-icons'
 
 export default function About() {
   return(
     <div className='about__section'>
-      <Container fluid>
-      <div className='wraper'>
-        <Row>
-            <Col md={3} sm={6}><div className='box box-a'>
+        <Container maxWidth={false}>
+        <Grid container spacing={0.5} justifyContent="center" alignItems="center">
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-a'>
                 <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faCloud} className="title__icon" />AWS</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                    <li><FontAwesomeIcon icon={faCloud} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-b'>
-                <div className='box__text'>
-                <li><FontAwesomeIcon icon={faChartLine} className="title__icon" />Advanced Analytics</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-b'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faChartLine} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-c'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faChartPie} className="title__icon" />Basic Analytics</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-a'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faChartPie} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-d'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faUserGroup} className="title__icon" />Consulting</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-b'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faUserGroup} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-        </Row>
-        <Row>
-            <Col md={3} sm={6}><div className='box box-b'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faShareNodes} className="title__icon" />Data Science</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-a'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faShareNodes} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-a'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faRobot} className="title__icon" />loT/Robotics</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-b'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faRobot} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-d'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faMobile} className="title__icon" />Mobile Application</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-a'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faMobile} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-            <Col md={3} sm={6}><div className='box box-c'>
-                <div className='box__text'>
-                  <li><FontAwesomeIcon icon={faWindowRestore} className="title__icon" />Web Application</li>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
-                  <div className="links">
-                    <a href='#' className='link'>See More</a>
+                </div> 
+              </div>
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+              <div className='box box-b'>
+              <div className='box__text'>
+                    <li><FontAwesomeIcon icon={faWindowRestore} className="title__icon" />AWS</li>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget cum sit commodo facilisis ac pellentesque morbi justo.</p>
+                    <div className="links">
+                      <a href='#' className='link'>See More</a>
                     </div>
-                </div>
-              </div></Col>
-        </Row>
-      </div>
+                </div> 
+              </div>
+              </Grid>   
+        </Grid>     
       </Container>
     </div>
   )
